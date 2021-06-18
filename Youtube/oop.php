082f21a939c5592c91d7219129e08db864621360
mahbubmr5001 <mahbubmr501@gmail.com>
2021-06-19 02:33:12 +0600

    19Jun2021

---
+++ Youtube/oop.php
@@ -0,0 +1,29 @@
+<?php 
+class person{
+
+    public string $name;
+    public string $surname;
+    private ? int $age;
+    public static int $counter =0;
+
+    public function __construct($name, $surname)
+    {
+        // echo $name.' '.$surname;
+        $this->name = $name;
+        $this->surname = $surname;
+        $this->age = null;
+        self::$counter++;        
+
+    }
+    public function setAge($age)
+    {
+        $this->age = $age;
+
+    }
+    public function getAge()
+    {
+         return $this->age;
+
+    }
+}
+ ?>
